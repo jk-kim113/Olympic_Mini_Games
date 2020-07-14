b96aa75e53e99849b2d204efbe160fb45d5ed373
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2020-07-15 00:52:10 +0900

    Fix Commit Error

---
+++ Olympic_Mini_Games/Assets/2_Scripts/PlayerAthletics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAthletics : MonoBehaviour
+{
+    Rigidbody mRB;
+
+    private void Awake()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        mRB.velocity = new Vector3(0, 0, 100);
+    }
+
+    public void StartButton()
+    {
+        Debug.Log("Click");
+        //mRB.AddForce( transform.forward * 100f);
+        //mRB.velocity = Vector3.forward * 100f;
+        mRB.velocity = new Vector3(0, 0, 10000);
+    }
+}
